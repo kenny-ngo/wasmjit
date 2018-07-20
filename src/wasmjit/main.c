83b1983c6bd2e89f1016e0a840972b0dd4513126
Rian Hunter <rian@alum.mit.edu>
2018-07-19 18:34:54 -0700

    Partially implement global section

--- src/wasmjit/main.c
+++ src/wasmjit/main.c
@@ -401,6 +401,26 @@ enum {
 	IMPORT_DESC_TYPE_GLOBAL,
 };
 
+struct GlobalType {
+	uint8_t valtype;
+	uint8_t mut;
+};
+
+int read_global_type(struct ParseState *pstate, struct GlobalType *globaltype)
+{
+	int ret;
+
+	ret = read_uint8_t(pstate, &globaltype->valtype);
+	if (!ret)
+		return 0;
+
+	ret = read_uint8_t(pstate, &globaltype->mut);
+	if (!ret)
+		return 0;
+
+	return 1;
+}
+
 struct ImportSection {
 	uint32_t n_imports;
 	struct ImportSectionImport {
@@ -411,10 +431,7 @@ struct ImportSection {
 			uint32_t typeidx;
 			struct Limits tabletype;
 			struct Limits memtype;
-			struct {
-				int valtype;
-				int mut;
-			} globaltype;
+			struct GlobalType globaltype;
 		} desc;
 	} *imports;
 };
@@ -484,22 +501,10 @@ int read_import_section(struct ParseState *pstate,
 				goto error;
 			break;
 		case IMPORT_DESC_TYPE_GLOBAL:
-			{
-				uint8_t valtype;
-				ret = read_uint8_t(pstate, &valtype);
-				if (!ret)
-					goto error;
-				import->desc.globaltype.valtype = valtype;
-			}
-
-			{
-				uint8_t mut;
-				ret = read_uint8_t(pstate, &mut);
-				if (!ret)
-					goto error;
-				import->desc.globaltype.mut = mut;
-			}
-
+			ret =
+			    read_global_type(pstate, &import->desc.globaltype);
+			if (!ret)
+				goto error;
 			break;
 		default:
 			goto error;
@@ -653,6 +658,302 @@ int read_memory_section(struct ParseState *pstate,
 	return 0;
 }
 
+enum {
+	/* Control Instructions */
+	OPCODE_UNREACHABLE = 0x00,
+	OPCODE_NOP = 0x01,
+	OPCODE_BLOCK = 0x02,
+	OPCODE_LOOP = 0x03,
+	OPCODE_IF = 0x04,
+	OPCODE_BR = 0x0C,
+	OPCODE_BR_IF = 0x0D,
+	OPCODE_BR_TABLE = 0x0E,
+	OPCODE_RETURN = 0x0F,
+	OPCODE_CALL = 0x10,
+	OPCODE_CALL_INDIRECT = 0x11,
+
+	/* Parametric Instructions */
+	OPCODE_DROP = 0x1A,
+	OPCODE_SELECT = 0x1B,
+
+	/* Variable Instructions */
+	OPCODE_GET_LOCAL = 0x20,
+	OPCODE_SET_LOCAL = 0x21,
+	OPCODE_TEE_LOCAL = 0x22,
+	OPCODE_GET_GLOBAL = 0x23,
+	OPCODE_SET_GLOBAL = 0x24,
+
+	/* Memory Instructions */
+	OPCODE_I32_LOAD = 0x28,
+	OPCODE_I64_LOAD = 0x29,
+	OPCODE_F32_LOAD = 0x2A,
+	OPCODE_F64_LOAD = 0x2B,
+	OPCODE_I32_LOAD8_S = 0x2C,
+	OPCODE_I32_LOAD8_U = 0x2D,
+	OPCODE_I32_LOAD16_S = 0x2E,
+	OPCODE_I32_LOAD16_U = 0x2F,
+	OPCODE_I64_LOAD8_S = 0x30,
+	OPCODE_I64_LOAD8_U = 0x31,
+	OPCODE_I64_LOAD16_S = 0x32,
+	OPCODE_I64_LOAD16_U = 0x33,
+	OPCODE_I64_LOAD32_S = 0x34,
+	OPCODE_I64_LOAD32_U = 0x35,
+	OPCODE_I32_STORE = 0x36,
+	OPCODE_I64_STORE = 0x37,
+	OPCODE_F32_STORE = 0x38,
+	OPCODE_F64_STORE = 0x39,
+	OPCODE_I32_STORE8 = 0x3A,
+	OPCODE_I32_STORE16 = 0x3B,
+	OPCODE_I64_STORE8 = 0x3C,
+	OPCODE_I64_STORE16 = 0x3D,
+	OPCODE_I64_STORE32 = 0x3E,
+	OPCODE_MEMORY_SIZE = 0x3F,
+	OPCODE_MEMORY_GROW = 0x40,
+
+	/* Numeric Instructions */
+	OPCODE_I32_CONST = 0x41,
+	OPCODE_I64_CONST = 0x42,
+	OPCODE_F32_CONST = 0x43,
+	OPCODE_F64_CONST = 0x44,
+
+	OPCODE_I32_EQZ = 0x45,
+	OPCODE_I32_EQ = 0x46,
+	OPCODE_I32_NE = 0x47,
+	OPCODE_I32_LT_S = 0x48,
+	OPCODE_I32_LT_U = 0x49,
+	OPCODE_I32_GT_S = 0x4A,
+	OPCODE_I32_GT_U = 0x4B,
+	OPCODE_I32_LE_S = 0x4C,
+	OPCODE_I32_LE_U = 0x4D,
+	OPCODE_I32_GE_S = 0x4E,
+	OPCODE_I32_GE_U = 0x4F,
+
+	OPCODE_I64_EQZ = 0x50,
+	OPCODE_I64_EQ = 0x51,
+	OPCODE_I64_NE = 0x52,
+	OPCODE_I64_LT_S = 0x53,
+	OPCODE_I64_LT_U = 0x54,
+	OPCODE_I64_GT_S = 0x55,
+	OPCODE_I64_GT_U = 0x56,
+	OPCODE_I64_LE_S = 0x57,
+	OPCODE_I64_LE_U = 0x58,
+	OPCODE_I64_GE_S = 0x59,
+	OPCODE_I64_GE_U = 0x5A,
+
+	OPCODE_F32_EQ = 0x5B,
+	OPCODE_F32_NE = 0x5C,
+	OPCODE_F32_LT = 0x5D,
+	OPCODE_F32_GT = 0x5E,
+	OPCODE_F32_LE = 0x5F,
+	OPCODE_F32_GE = 0x60,
+
+	OPCODE_F64_EQ = 0x61,
+	OPCODE_F64_NE = 0x62,
+	OPCODE_F64_LT = 0x63,
+	OPCODE_F64_GT = 0x64,
+	OPCODE_F64_LE = 0x65,
+	OPCODE_F64_GE = 0x66,
+
+	OPCODE_I32_CLZ = 0x67,
+	OPCODE_I32_CTZ = 0x68,
+	OPCODE_I32_POPCNT = 0x69,
+	OPCODE_I32_ADD = 0x6A,
+	OPCODE_I32_SUB = 0x6B,
+	OPCODE_I32_MUL = 0x6C,
+	OPCODE_I32_DIV_S = 0x6D,
+	OPCODE_I32_DIV_U = 0x6E,
+	OPCODE_I32_REM_S = 0x6F,
+	OPCODE_I32_REM_U = 0x70,
+	OPCODE_I32_AND = 0x71,
+	OPCODE_I32_OR = 0x72,
+	OPCODE_I32_XOR = 0x73,
+	OPCODE_I32_SHL = 0x74,
+	OPCODE_I32_SHR_S = 0x75,
+	OPCODE_I32_SHR_U = 0x76,
+	OPCODE_I32_ROTL = 0x77,
+	OPCODE_I32_ROTR = 0x78,
+
+	OPCODE_I64_CLZ = 0x79,
+	OPCODE_I64_CTZ = 0x7A,
+	OPCODE_I64_POPCNT = 0x7B,
+	OPCODE_I64_ADD = 0x7C,
+	OPCODE_I64_SUB = 0x7D,
+	OPCODE_I64_MUL = 0x7E,
+	OPCODE_I64_DIV_S = 0x7F,
+	OPCODE_I64_DIV_U = 0x80,
+	OPCODE_I64_REM_S = 0x81,
+	OPCODE_I64_REM_U = 0x82,
+	OPCODE_I64_AND = 0x83,
+	OPCODE_I64_OR = 0x84,
+	OPCODE_I64_XOR = 0x85,
+	OPCODE_I64_SHL = 0x86,
+	OPCODE_I64_SHR_S = 0x87,
+	OPCODE_I64_SHR_U = 0x88,
+	OPCODE_I64_ROTL = 0x89,
+	OPCODE_I64_ROTR = 0x8A,
+
+	OPCODE_F32_ABS = 0x8B,
+	OPCODE_F32_NEG = 0x8C,
+	OPCODE_F32_CEIL = 0x8D,
+	OPCODE_F32_FLOOR = 0x8E,
+	OPCODE_F32_TRUNC = 0x8F,
+	OPCODE_F32_NEAREST = 0x90,
+	OPCODE_F32_SQRT = 0x91,
+	OPCODE_F32_ADD = 0x92,
+	OPCODE_F32_SUB = 0x93,
+	OPCODE_F32_MUL = 0x94,
+	OPCODE_F32_DIV = 0x95,
+	OPCODE_F32_MIN = 0x96,
+	OPCODE_F32_MAX = 0x97,
+	OPCODE_F32_COPYSIGN = 0x98,
+
+	OPCODE_F64_ABS = 0x99,
+	OPCODE_F64_NEG = 0x9A,
+	OPCODE_F64_CEIL = 0x9B,
+	OPCODE_F64_FLOOR = 0x9C,
+	OPCODE_F64_TRUNC = 0x9D,
+	OPCODE_F64_NEAREST = 0x9E,
+	OPCODE_F64_SQRT = 0x9F,
+	OPCODE_F64_ADD = 0xA0,
+	OPCODE_F64_SUB = 0xA1,
+	OPCODE_F64_MUL = 0xA2,
+	OPCODE_F64_DIV = 0xA3,
+	OPCODE_F64_MIN = 0xA4,
+	OPCODE_F64_MAX = 0xA5,
+	OPCODE_F64_COPYSIGN = 0xA6,
+
+	OPCODE_I32_WRAP_I64 = 0xA7,
+	OPCODE_I32_TRUNC_S_F32 = 0xA8,
+	OPCODE_I32_TRUNC_U_F32 = 0xA9,
+	OPCODE_I32_TRUNC_S_F64 = 0xAA,
+	OPCODE_I32_TRUNC_U_F64 = 0xAB,
+	OPCODE_I64_EXTEND_S_I32 = 0xAC,
+	OPCODE_I64_EXTEND_U_I32 = 0xAD,
+	OPCODE_I64_TRUNC_S_F32 = 0xAE,
+	OPCODE_I64_TRUNC_U_F32 = 0xAF,
+	OPCODE_I64_TRUNC_S_F64 = 0xB0,
+	OPCODE_I64_TRUNC_U_F64 = 0xB1,
+	OPCODE_F32_CONVERT_S_I32 = 0xB2,
+	OPCODE_F32_CONVERT_U_I32 = 0xB3,
+	OPCODE_F32_CONVERT_U_I64 = 0xB4,
+	OPCODE_F32_CONVERT_S_I64 = 0xB5,
+	OPCODE_F32_DEMOTE_F64 = 0xB6,
+	OPCODE_F64_CONVERT_S_I32 = 0xB7,
+	OPCODE_F64_CONVERT_U_I32 = 0xB8,
+	OPCODE_F64_CONVERT_U_I64 = 0xB9,
+	OPCODE_F64_CONVERT_S_I64 = 0xBA,
+	OPCODE_F64_PROMOTE_F32 = 0xBB,
+	OPCODE_I32_REINTERPRET_F32 = 0xBC,
+	OPCODE_I64_REINTERPRET_F64 = 0xBD,
+	OPCODE_F32_REINTERPRET_I32 = 0xBE,
+	OPCODE_F64_REINTERPRET_I46 = 0xBF,
+};
+
+struct Instr {
+	uint8_t opcode;
+	union {
+		struct {
+			struct {
+				uint8_t is_empty;
+				uint8_t valtype;
+			} blocktype;
+			uint32_t n_instructions;
+			struct Instr *instructions;
+		} block, loop;
+		struct {
+			struct {
+				uint8_t is_empty;
+				uint8_t valtype;
+			} blocktype;
+			uint32_t n_instructions_then;
+			struct Instr *instructions_then;
+			uint32_t n_instructions_else;
+			struct Instr *instructions_else;
+		} if_;
+		struct {
+			int labelidx;
+		} br, br_if;
+		struct {
+			uint32_t n_labelidxs;
+			int *labelidxs;
+			int labelidx;
+		} br_table;
+		/* TODO: add the rest */
+	} data;
+};
+
+int read_instruction(struct ParseState *pstate, struct Instr *instr)
+{
+	(void)pstate;
+	(void)instr;
+	return 0;
+}
+
+struct GlobalSection {
+	uint32_t n_globals;
+	struct GlobalSectionGlobal {
+		struct GlobalType type;
+		uint32_t n_instructions;
+		struct Instr *instructions;
+	} *globals;
+};
+
+int read_global_section(struct ParseState *pstate,
+			struct GlobalSection *global_section)
+{
+	int ret;
+
+	global_section->globals = NULL;
+
+	ret = read_uleb_uint32_t(pstate, &global_section->n_globals);
+	if (!ret)
+		goto error;
+
+	if (global_section->n_globals) {
+		uint32_t i;
+
+		global_section->globals =
+		    calloc(global_section->n_globals,
+			   sizeof(struct GlobalSectionGlobal));
+		if (!global_section->globals)
+			goto error;
+
+		for (i = 0; i < global_section->n_globals; ++i) {
+			struct GlobalSectionGlobal *global =
+			    &global_section->globals[i];
+
+			ret = read_global_type(pstate, &global->type);
+			if (!ret)
+				goto error;
+
+			global->n_instructions = 0;
+			while (1) {
+				struct Instr instruction, *next_instructions;
+				ret = read_instruction(pstate, &instruction);
+				if (!ret)
+					goto error;
+
+				++global->n_instructions;
+
+				next_instructions =
+				    realloc(global->instructions,
+					    global->n_instructions);
+				if (!next_instructions) {
+					goto error;
+				}
+
+				global->instructions = next_instructions;
+			}
+		}
+	}
+
+	return 1;
+
+ error:
+	/* TODO: free global_section memory */
+	return 0;
+}
+
 int main(int argc, char *argv[])
 {
 	int ret;
@@ -662,6 +963,7 @@ int main(int argc, char *argv[])
 	struct FunctionSection function_section;
 	struct TableSection table_section;
 	struct MemorySection memory_section;
+	struct GlobalSection global_section;
 
 	init_pstate(&pstate);
 
@@ -761,6 +1063,11 @@ int main(int argc, char *argv[])
 			     &memory_section);
 			break;
 		case SECTION_ID_GLOBAL:
+			/*
+			   READ("global section", read_global_section,
+			   &global_section);
+			   break;
+			 */
 		case SECTION_ID_EXPORT:
 		case SECTION_ID_START:
 		case SECTION_ID_ELEMENT:
