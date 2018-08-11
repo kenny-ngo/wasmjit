339058cb05da5c85e5cbedf58914cdd21f2a1e86
Rian Hunter <rian@alum.mit.edu>
2018-08-11 13:38:06 -0700

    Add build helper

---
+++ build_emscripten.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -eu
+
+make clean
+
+make wasmjit
+
+./wasmjit "$1" > "$1.o"
+
+SUPPORT="static_runtime emscripten_runtime_posix runtime vector static_emscripten_runtime"
+SUPPORT_FILES=""
+for FILE in $SUPPORT
+do
+    rm -f src/wasmjit/${FILE}.o
+    make src/wasmjit/${FILE}.o
+    SUPPORT_FILES="$SUPPORT_FILES src/wasmjit/${FILE}.o"
+done
+
+
+cc -o "$1.exe" "$1.o" $SUPPORT_FILES
